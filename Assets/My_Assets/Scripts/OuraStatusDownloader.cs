9ab63d5ce89d0c8e4701e84365a262f3a03a2ceb
GONBEEEproject <gonbeee.project@gmail.com>
2022-05-26 02:47:19 +0900

    OuraRingAPIv2叩く方法完全に理解した

---
+++ Assets/My_Assets/Scripts/OuraStatusDownloader.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class OuraStatusDownloader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetStatus());
+    }
+
+    private IEnumerator GetStatus()
+    {
+
+        using (var req = UnityWebRequest.Get("https://api.ouraring.com/v2/usercollection/personal_info"))
+        {
+            req.SetRequestHeader("host", "api.ouraring.com");
+            req.SetRequestHeader("authorization", "Bearer " + StaticTokenHolder.TOKEN);
+
+            yield return req.SendWebRequest();
+
+            try
+            {
+                Debug.Log(req.downloadHandler.text);
+            }
+            catch
+            {
+                Debug.Log(req.error);
+            }
+        }
+
+        using (var req = UnityWebRequest.Get("https://cloud.ouraring.com/v2/usercollection/daily_activity"))
+        {
+            req.SetRequestHeader("host", "api.ouraring.com");
+            req.SetRequestHeader("authorization", "Bearer " + StaticTokenHolder.TOKEN);
+
+            yield return req.SendWebRequest();
+
+            try
+            {
+                Debug.Log(req.downloadHandler.text);
+            }
+            catch
+            {
+                Debug.Log(req.error);
+            }
+        }
+
+        using (var req = UnityWebRequest.Get("https://cloud.ouraring.com/v2/usercollection/heartrate"))
+        {
+            req.SetRequestHeader("host", "api.ouraring.com");
+            req.SetRequestHeader("authorization", "Bearer "+StaticTokenHolder.TOKEN);
+
+            yield return req.SendWebRequest();
+
+            try
+            {
+                Debug.Log(req.downloadHandler.text);
+            }
+            catch
+            {
+                Debug.Log(req.error);
+            }
+        }
+    }
+}
